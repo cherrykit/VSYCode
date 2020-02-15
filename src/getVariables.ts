304e4b0a36a379598b279a2d88fd38ba043e53ae
Sophia Pietsch <cherrykit@outlook.com>
2020-02-15 10:30:23 -0500

    Added recursive JSON generation.

--- src/getVariables.ts
+++ src/getVariables.ts
@@ -15,7 +15,35 @@ export default async function getVariables(varName: string) {
 	//const response3 = await session.customRequest('variables', { variablesReference: ref });
 
 	const response1 = await session.customRequest('evaluate', { expression: varName, frameId: frameId });
-	const variable = response1.result;
+
+	if (response1.type != "Object") {
+		return response1.result;
+	}
+
+	const response2 = await session.customRequest('variables', { variablesReference: response1.variablesReference});
+
+	const variable = await parseVariable(session, "", response2.variables[0]);
 
 	return variable;
+}
+
+async function parseVariable(session: vscode.DebugSession, string : string, response : any) {
+
+	if (response.name == "__proto__" || response.type != "Object") {
+		return string;
+	}
+
+	//modify string to incorporate info from current object
+
+	string += response.value;
+
+
+	const response1 = await session.customRequest('variables', { variablesReference: response.variablesReference}); 
+
+	for (let i = 0; i < response1.variables.length; i++) {
+		string = await parseVariable(session, string, response1.variables[i]);
+	}
+
+	return string;
+
 }
